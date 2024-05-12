eac21de50b8abde30c1f3e5c9ec6589a93c06b5e
philipmogul <philipp1mogul@gmail.com>
2024-05-13 00:49:54 +0300

    Jquery and css3 animations

---
+++ jquery_animations.php
@@ -0,0 +1,29 @@
+<?php 
+
+// Performing jquery and css3 animations
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Jquery animations</title>
+</head>
+<body>
+	<center><h2>jQuery Animations</h2></center><hr />
+
+
+
+
+
+
+
+
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="jquery.js"></script>
+</body>
+</html>
